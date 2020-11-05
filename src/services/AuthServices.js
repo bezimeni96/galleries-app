badae637915a0b3f5f2841deb376f589b8daf997
Dusan Vostic <vosticdusan@gmail.com>
2020-11-05 19:04:26 +0100

    ispravljena greska oko axios interceptors-a.

--- src/services/AuthServices.js
+++ src/services/AuthServices.js
@@ -1,19 +1,7 @@
 import AxiosServices from './AxiosServices';
 
 export default class AuthService extends AxiosServices {
-  constructor() {
-    super();
-    this.apiClient.interceptors.request.use( config => {
-      const token = localStorage.getItem('token');
-        console.log('usao', token);
-        config.headers["Authorization"] = token;
-      return config;
-    },
-      error => {
-        console.log('erooooor')
-        return Promise.reject(error)
-      })
-  }
+  
 
   login(credentials) {
     return this.apiClient.post('/login', credentials)
--- src/services/AxiosServices.js
+++ src/services/AxiosServices.js
@@ -6,6 +6,18 @@ export default class AxiosService {
       baseURL: 'http://localhost:8000/api',
       timeout: 5000,
       headers: { Accept: 'application/json' }
-    })
+    });
+    this.apiClient.interceptors.request.use( config => {
+      const token = localStorage.getItem('token');
+        console.log('usao', token);
+        config.headers = {
+          "Authorization":  `Bearer ${token}`
+        }
+        return config;
+    },
+      error => {
+        console.log('erooooor')
+        return Promise.reject(error)
+    });
   }
 }
